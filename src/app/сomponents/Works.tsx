ad3510742473d96f9791629a6e8a927cdf91665d
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-12-03 12:26:12 +0200

    Add pagenator

--- src/app/сomponents/Works.tsx
+++ src/app/сomponents/Works.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { ProjectsShortInfo } from "./ProjectsShortInfo";
 import { ProjectInfo } from "./ProjectInfo";
+import Image from "next/image";
 
 export const Works = () => {
   return (
     <div className="w-full flex justify-end items-center mt-[74px]">
-      <div className="w-[1230px] flex flex-col items-start">
+      <div className="w-[1230px] flex flex-col items-start relative">
         <h1 className="font-semibold text-[64px] leading-[78px] mb-[40px]">
           Works-Set
         </h1>
         <ProjectsShortInfo />
-        <ProjectInfo/>
+        <ProjectInfo />
+        <Image
+          src="/paginator_3.svg"
+          alt="Paginator"
+          width={23}
+          height={209}
+          className="absolute"
+          style={{ top: "465px", right: "213px" }}
+        />
       </div>
     </div>
   );
