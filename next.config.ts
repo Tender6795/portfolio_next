1ea60d001fd760255893ace13a700067e8af2029
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-12-03 11:21:03 +0200

    Change next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, 
-  poweredByHeader: false, 
+  reactStrictMode: true,
+  poweredByHeader: false,
   devIndicators: {
     buildActivity: false, 
   },
+  productionBrowserSourceMaps: false, 
 };
 
 export default nextConfig;
