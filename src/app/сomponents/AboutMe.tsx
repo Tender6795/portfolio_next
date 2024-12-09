b272ff34a0feeb3c998f602705a926ebf38fd4e5
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-12-09 10:28:51 +0200

    Adaptive in progress

--- src/app/сomponents/AboutMe.tsx
+++ src/app/сomponents/AboutMe.tsx
@@ -11,7 +11,7 @@ export const AboutMe = () => {
     );
   };
 
-  const downloadHanlde = () => {
+  const downloadHandle = () => {
     const link = document.createElement("a");
     link.href = "/CV.pdf";
     link.download = "CV.pdf";
@@ -19,31 +19,31 @@ export const AboutMe = () => {
   };
 
   return (
-    <div className="flex justify-end items-center">
-      <div className="w-[784px] h-[271px] mr-[-174px]">
-        <h1 className="font-semibold text-[64px] leading-[78px]">
+    <div className="flex flex-col-reverse lg:flex-row items-center lg:justify-end gap-6 lg:gap-0">
+      <div className="lg:w-[54.44%] lg:h-[16.88rem] lg:mr-[-12.08%] text-center lg:text-left">
+        <h1 className="font-semibold text-responsive-h1 leading-responsive-h1">
           {`Hello, I’m Illia, `}
           <br />
           {`a Full-Stack Developer`}
         </h1>
-        <div className="w-[600px]">
-          <span className="font-normal text-[14px] leading-[17px] block">
+        <div className="w-full lg:w-[80%] mx-auto lg:mx-0">
+          <p className="font-normal text-responsive-p leading-responsive-p block">
             I&apos;m a web developer with over 5 years of expertise in
             Full-Stack development, specializing in the MERN stack (MongoDB,
             Express, ReactJS, and NodeJS). Throughout my career, I have created
             numerous robust web apps, focusing on clean, reusable code and best
-            practices..
-          </span>
-          <span className="font-normal text-[14px] leading-[17px] mt-[30px] block">
+            practices.
+          </p>
+          <p className="font-normal text-responsive-p leading-responsive-p mt-[2rem] block">
             I’ve developed solutions that drive business success and can
             recommend platforms, languages, and features to elevate your
             project. Let’s make it even better - feel free to reach out!
-          </span>
-          <div className="flex gap-[20px] mt-[60px]">
+          </p>
+          <div className="flex flex-col sm:flex-row gap-[1.25rem] mt-[4rem]">
             <Button text="Get in touch" onClick={getTouchHandle} />
             <Button
               text="Download CV"
-              onClick={downloadHanlde}
+              onClick={downloadHandle}
               className="bg-[#363636]"
             />
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,19 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        'responsive-h1': '4vw',
+        'responsive-p': 'clamp(0.75rem, 1.5vw, 1.125rem)',
+      },
+      lineHeight: {
+        'responsive-h1': 'clamp(4.5vw, 6.5rem, 7.5rem)',
+        'responsive-p': 'clamp(1.25rem, 2vw, 1.375rem)',
+      },
+      screens: {
+        'lg': '1024px',
+        'xl': '1440px',
+        '2xl': '1920px',
+      },
     },
   },
   plugins: [],
