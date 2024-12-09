f6b301eb071aa17b1ff7f54d1616e8c1199c1473
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-12-09 18:12:18 +0200

    Adaptive in progress

--- src/app/сomponents/Skills.tsx
+++ src/app/сomponents/Skills.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import { Skill } from "../ui/Skill";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export const Skills = () => {
   const skills = [
@@ -23,19 +23,19 @@ export const Skills = () => {
 
   return (
     <div className="w-full flex justify-end items-center mt-[130px]">
-      <div className="w-full flex justify-between">
-        <div className="w-[926px] flex flex-col items-start">
+      <div className="w-full flex justify-between items-start">
+        <div className="flex flex-col items-start w-full md:w-[calc(100%-304px)]">
           <h1 className="font-semibold text-[64px] leading-[78px] mb-[40px]">
             Skill-Set
           </h1>
-          <div className="flex flex-wrap gap-x-[20px] gap-y-[40px]">
+          <div className="flex flex-wrap gap-x-[20px] gap-y-[40px] w-full">
             {skills.map((skill, index) => (
               <motion.div
                 key={index}
-                className="w-[30%]"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: index * 0.1 }}
+                className="flex-grow"
               >
                 <Skill skillName={skill} />
               </motion.div>
@@ -48,7 +48,7 @@ export const Skills = () => {
             alt="Paginator"
             width={23}
             height={209}
-            className="absolute top-[317px]"
+            className="absolute top-[317px] right-[213px]"
           />
         </div>
       </div>
