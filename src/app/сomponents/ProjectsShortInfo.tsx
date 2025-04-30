0aab5dc23f325a1b3a93b885b67f2dbcf44d37f2
Muchnik Illia <muchnic.illia.js@gmail.com>
2025-04-30 12:25:24 +0300

    Adaptive in progress

--- src/app/сomponents/ProjectsShortInfo.tsx
+++ src/app/сomponents/ProjectsShortInfo.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Devider } from "../ui/Devider";
+import { useIsDesktop } from "../hooks/useIsDesktop";
 
 export const ProjectsShortInfo = () => {
+  const isDesktop = useIsDesktop(1500);
+
   return (
-    <div className="w-[88.75%] h-[100px] bg-[#03A4734D] rounded-[20px] flex mb-[90px]">
-      <div className="flex-1 flex items-center justify-start pl-[20px]">
-        <div className="flex flex-row items-center mr-[20px]">
-          <div className="text-[57.6px] font-[600] leading-[70.22px] text-left font-montserrat mr-[10px]">
+    <div
+      className={`h-[100px] bg-[#03A4734D] rounded-[20px] flex mb-[90px] ${
+        isDesktop ? "w-[88.75%]" : "w-full"
+      }`}
+    >
+      <div className="flex-1 flex items-center justify-start pl-[2vw]">
+        <div className="flex flex-row items-center mr-[1vw]">
+          <div className="text-[5vw] md:text-[3vw] lg:text-[2.5vw] font-[600] leading-[calc(text-size*1.2)] text-left font-montserrat mr-[0.5vw]">
             10+
           </div>
-          <div className="text-[21.6px] font-[600] leading-[23.4px] text-left font-montserrat mr-[20px]">
-            successful  
+          <div className="text-[1.8vw] md:text-[1.2vw] lg:text-[0.9vw] font-[600] leading-[calc(text-size*1.1)] text-left font-montserrat mr-[1vw]">
+            successful
             <br />
             projects
           </div>
@@ -19,13 +26,13 @@ export const ProjectsShortInfo = () => {
 
       <Devider />
 
-      <div className="flex-1 flex items-center justify-start pl-[20px]">
-        <div className="flex flex-row items-center mr-[20px]">
-          <div className="text-[57.6px] font-[600] leading-[70.22px] text-left font-montserrat mr-[10px]">
+      <div className="flex-1 flex items-center justify-start pl-[2vw]">
+        <div className="flex flex-row items-center mr-[1vw]">
+          <div className="text-[5vw] md:text-[3vw] lg:text-[2.5vw] font-[600] leading-[calc(text-size*1.2)] text-left font-montserrat mr-[0.5vw]">
             1000+
           </div>
-          <div className="text-[21.6px] font-[600] leading-[23.4px] text-left font-montserrat mr-[20px]">
-            hours  
+          <div className="text-[1.8vw] md:text-[1.2vw] lg:text-[0.9vw] font-[600] leading-[calc(text-size*1.1)] text-left font-montserrat mr-[1vw]">
+            hours
             <br />
             of work
           </div>
@@ -34,13 +41,13 @@ export const ProjectsShortInfo = () => {
 
       <Devider />
 
-      <div className="flex-1 flex items-center justify-start pl-[20px]">
+      <div className="flex-1 flex items-center justify-start pl-[2vw]">
         <div className="flex flex-row items-center">
-          <div className="text-[57.6px] font-[600] leading-[70.22px] text-left font-montserrat mr-[10px]">
+          <div className="text-[5vw] md:text-[3vw] lg:text-[2.5vw] font-[600] leading-[calc(text-size*1.2)] text-left font-montserrat mr-[0.5vw]">
             $300K+
           </div>
-          <div className="text-[21.6px] font-[600] leading-[23.4px] text-left font-montserrat">
-            earned  
+          <div className="text-[1.8vw] md:text-[1.2vw] lg:text-[0.9vw] font-[600] leading-[calc(text-size*1.1)] text-left font-montserrat">
+            earned
             <br />
             by clients
           </div>
--- src/app/сomponents/Works.tsx
+++ src/app/сomponents/Works.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { ProjectsShortInfo } from "./ProjectsShortInfo";
 import { ProjectInfo } from "./ProjectInfo";
 import Image from "next/image";
+import { useIsDesktop } from "../hooks/useIsDesktop";
 
 export const Works = () => {
-  
+  const isDesktop = useIsDesktop(1500);
+
   return (
     <div className="w-full flex justify-end items-center mt-[74px]">
       <div className="w-full flex flex-col items-start relative">
@@ -13,14 +15,16 @@ export const Works = () => {
         </h1>
         <ProjectsShortInfo />
         <ProjectInfo />
-        <Image
-          src="/paginator_3.svg"
-          alt="Paginator"
-          width={23}
-          height={209}
-          className="absolute"
-          style={{ top: "465px", right: "213px" }}
-        />
+        {!!isDesktop && (
+          <Image
+            src="/paginator_3.svg"
+            alt="Paginator"
+            width={23}
+            height={209}
+            className="absolute"
+            style={{ top: "465px", right: "213px" }}
+          />
+        )}
       </div>
     </div>
   );
